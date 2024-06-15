30d94af6849ca9e12b6c0bc3237483031db24265
DrPrime01 <gabrielshoyombo2002@gmail.com>
2024-06-15 05:26:35 +0100

    feat: added styledimagecontainer component

--- components/StyledImageContainer.tsx
+++ components/StyledImageContainer.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+
+import Avatar from "@/public/images/Avatar.png";
+
 export default function StyledImageContainer() {
-	return <div className=""></div>;
+	return (
+		<div className="relative h-[300px] w-[200px] horizontal-grad-border-container">
+			<Image
+				src={Avatar}
+				alt="profile"
+				className="object-cover horizontal-grad-border w-full h-full"
+			/>
+		</div>
+	);
 }
