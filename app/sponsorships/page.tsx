c9dc5f1ee112c023a28bfd6984a8c31fd36d6736
Oluwatobi <samsonadebowale890@gmail.com>
2024-06-20 08:23:15 +0100

    ...

--- app/sponsorships/page.tsx
+++ app/sponsorships/page.tsx
@@ -6,14 +6,16 @@ import heroImage from "@/public/images/sponsorship-img.png";
 export default function Sponsorships() {
   return (
     <section className="gradient-bg">
-      <main className="flex relative z-10 my-[200px] mx-auto flex-col items-center w-4/5">
-        <div>
-          <h1>Become a Sponsor of FlutterBytes Conference 2024</h1>
+      <main className="flex relative z-10 px-4 py-32 md:py-[180px] flex-col items-center md:w-4/5 sm:w-full mx-0 sm:mx-auto">
+        <div className="mb-12 border-y-2 border-fbc_blue-100 py-[10px]">
+          <h1 className="text-gradient text-center text-xl md:text-3xl font-semibold">
+            Become a Sponsor of FlutterBytes Conference 2024!
+          </h1>
         </div>
         <Image
           src={heroImage}
-          alt="Image of some of our sponsors on our merch"
-          className="transition-all duration-500 h-6 md:h-auto w-[122.53px] md:w-auto"
+          alt="Image of some of our sponsors on the FlutterBytes Conference merch"
+          className="mb-12 md:h-auto sm:max-w-[80%]"
         />
         <div className="flex flex-col items-center max-w-[700px]">
           <div>
@@ -29,9 +31,15 @@ export default function Sponsorships() {
               packages and opportunities.{" "}
             </p>
           </div>
-          <div>
-            <Button children={"Send us a mail"} />
-            <Button children={"Copy phone number"} />
+          <div className="flex w-full flex-col justify-center sm:sm:flex-row gap-2 mt-8">
+            <Button
+              children={"Send us a mail"}
+			  otherstyles="flex justify-center w-full sm:w-fit"
+            />
+            <Button
+              children={"Copy phone number"}
+              otherstyles="flex w-full bg-transparent text-[#2A9DF4] border-[1.5px] border-[#2A9DF4] justify-center sm:w-fit"
+            />
           </div>
         </div>
       </main>
