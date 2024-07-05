6074fe351df6e008186ecc7095c823c4d5740451
Abdrahman Oladimeji <abdrahmanoladimeji02@gmail.com>
2024-07-06 00:33:34 +0100

    update: responsiveness of about and apply to speak page implemented

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,11 +4,11 @@ import { RightArrow2 } from "@/components/vectors/RightArrow";
 
 export default function About() {
 	return (
-		<section className="min-h-screen gradient-no-grid max-w-[920px] mx-auto w-full">
-			<h2 className="font-gigaSans flex mx-auto w-fit text-center mt-[140px] px-5 border-y-[1.5px] border-fbc_blue-100 text-[32px] font-semibold relative z-10 heading-text">
+		<section className="flex flex-col min-h-screen gradient-no-grid max-w-[920px] mx-auto w-[calc(100%_-_32px)] sm:w-[calc(100%_-_50px)] md:w-[calc(100%_-_64px)]">
+			<h2 className="font-gigaSans flex mx-auto w-fit text-center mt-[120px] md:mt-[140px] px-5 border-y-[1.5px] border-fbc_blue-100 text-xl md:text-[32px] font-semibold relative z-10 heading-text">
 				About us
 			</h2>
-			<div className="font-gigaSans mt-[52px] mb-[100px] p-5 md:px-[80px] xl:px-[111px] bg-fbc_blue-100 border-[0.5px] border-blue-100 text-blue-500 text-sm rounded-2xl relative z-10 space-y-5">
+			<div className="font-gigaSans mt-8 md:mt-[52px] mb-[100px] py-3 px-[18px] md:py-5 md:px-[80px] xl:px-[111px] bg-fbc_blue-100 border-[0.5px] border-blue-100 text-blue-500 text-sm rounded-2xl relative z-10 space-y-5">
 				<p>
 					At FlutterBytes Conference, we&rsquo;re dedicated to creating a
 					premier event experience that connects professionals, thought leaders,
