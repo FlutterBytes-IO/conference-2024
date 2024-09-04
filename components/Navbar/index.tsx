560291946efa5452187456546697ca40045f7326
DrPrime01 <drprime.dev@gmail.com>
2024-09-04 16:37:04 +0100

    feat: added hackathon link

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -41,7 +41,7 @@ const Navbar = forwardRef<HTMLDivElement, NavbarProps>(
         ref={ref}
         className={`px-5 md:px-10 top-0 bg-opacity-[10%] backdrop-blur-[10px] ${
           scrollPosition! > 50 ? "bg-white" : "bg-transparent"
-        } py-5 transition-all z-30 duration-500 flex items-center justify-between fixed w-screen ${
+        } py-5 transition-all z-30 duration-500 flex items-center gap-x-10 justify-between fixed w-screen ${
           isDarkmode
             ? "border-[#1167B1] hero-grad-dark"
             : "border-[#D0EFFF] bg-transparent"
@@ -49,7 +49,7 @@ const Navbar = forwardRef<HTMLDivElement, NavbarProps>(
       >
         <Link
           href="/"
-          className="relative max-lg:after:hidden after:absolute after:content-[''] after:left-full after:top-1/2 after:-translate-y-1/2 after:w-[170%] after:overflow-hidden after:h-px after:bg-fbc_blue-100"
+          className="relative max-xl:after:hidden after:absolute after:content-[''] after:left-full after:top-1/2 after:-translate-y-1/2 after:w-[170%] after:overflow-hidden after:h-px after:bg-fbc_blue-100"
         >
           <Image
             src={isDarkmode ? NavLogoDark : NavLogoLight}
@@ -92,6 +92,15 @@ const Navbar = forwardRef<HTMLDivElement, NavbarProps>(
                 Apply to speak
                 <RightArrow2 color={!isDarkmode ? "#879CAA" : ""} />
               </Link>
+              <Link
+                href="https://drive.google.com/file/d/1fy53GKvSgOuhk6ZqjcWthuhFCHVomHve/view?usp=sharing"
+                className={`flex items-center py-1.5 px-1 w-[130px] justify-between text-[11px] font-semibold ${
+                  isDarkmode ? "text-blue-300" : "text-neutral-500"
+                }`}
+              >
+                Hackathon
+                <RightArrow2 color={!isDarkmode ? "#879CAA" : ""} />
+              </Link>
             </div>
             <div
               className={`flex flex-col gap-y-[9px] py-2 ml-1 xs:ml-0 pl-2 xs:pl-3 border-l ${
@@ -130,7 +139,7 @@ const Navbar = forwardRef<HTMLDivElement, NavbarProps>(
             href="/volunteer"
             className={`${
               isDarkmode ? "text-neutral-400" : "text-neutral-500"
-            } py-1 px-3 rounded-[40px] text-sm font-semibold leading-6`}
+            } py-1 px-3 rounded-[40px] text-sm font-semibold leading-6 text-center`}
           >
             Apply to volunteer
           </Link>
@@ -138,7 +147,7 @@ const Navbar = forwardRef<HTMLDivElement, NavbarProps>(
             href="/speakers-application"
             className={`${
               isDarkmode ? "text-neutral-400" : "text-neutral-500"
-            } py-1 px-3 rounded-[40px] text-sm font-semibold leading-6`}
+            } py-1 px-3 rounded-[40px] text-sm font-semibold leading-6 text-center`}
           >
             Apply to speak
           </Link>
@@ -146,20 +155,28 @@ const Navbar = forwardRef<HTMLDivElement, NavbarProps>(
             href="/sponsorships"
             className={`${
               isDarkmode ? "text-neutral-400" : "text-neutral-500"
-            } py-1 px-3 rounded-[40px] text-sm font-semibold leading-6`}
+            } py-1 px-3 rounded-[40px] text-sm font-semibold leading-6 text-center`}
           >
             Be a sponsor
+          </Link>
+          <Link
+            href="https://drive.google.com/file/d/1fy53GKvSgOuhk6ZqjcWthuhFCHVomHve/view?usp=sharing"
+            className={`${
+              isDarkmode ? "text-neutral-400" : "text-neutral-500"
+            } py-1 px-3 rounded-[40px] text-sm font-semibold leading-6 text-center`}
+          >
+            Hackathon
           </Link>
           <Link
             href="/about"
             className={`${
               isDarkmode ? "text-neutral-400" : "text-neutral-500"
-            } py-1 px-3 rounded-[40px] text-sm font-semibold leading-6`}
+            } py-1 px-3 rounded-[40px] text-sm font-semibold leading-6 text-center`}
           >
             About us
           </Link>
         </div>
-        <div className="hidden lg:flex items-center gap-x-3 relative before:max-lg:hidden before:absolute before:content-[''] before:right-full before:top-1/2 before:-translate-y-1/2 before:w-[170%] before:h-px before:bg-fbc_blue-100">
+        <div className="hidden lg:flex items-center gap-x-3 relative before:max-xl:hidden before:absolute before:content-[''] before:right-full before:top-1/2 before:-translate-y-1/2 before:w-[170%] before:h-px before:bg-fbc_blue-100">
           <Button
             component="link"
             href="https://eventprime.co/e/flutter-bytes-conference-2024"
