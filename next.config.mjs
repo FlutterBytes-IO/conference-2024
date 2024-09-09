74e840f9940bf0d5bccf6a54b61b9b33c4ee6ecc
DrPrime01 <drprime.dev@gmail.com>
2024-09-09 10:56:45 +0100

    fix: image issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     remotePatterns: [
       {
@@ -11,6 +12,7 @@ const nextConfig = {
         hostname: "i.ibb.co",
       },
     ],
+    unoptimized: true,
   },
 };
 
